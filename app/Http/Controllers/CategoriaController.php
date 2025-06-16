abfa95e467c5672562547646a0542a84cadfefec
albertosanchezc <albertosanchezc98@gmail.com>
2025-06-15 21:51:08 -0600

    Creando la api de categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
     //
+    public function index()
+    {
+        return response()->json(['categorias' => Categoria::all()]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/categorias', [CategoriaController::class, 'index']);
