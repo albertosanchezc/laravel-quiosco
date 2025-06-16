fad1d6dcfd48f948acdb6b701bf2742fbf5d72b6
albertosanchezc <albertosanchezc98@gmail.com>
2025-06-15 21:57:51 -0600

    Creando un resource y definiendo que se mostrará en la api

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoriaCollection;
 use App\Models\Categoria;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,8 @@ class CategoriaController extends Controller
     //
     public function index()
     {
-        return response()->json(['categorias' => Categoria::all()]);
+        // return response()->json(['categorias' => Categoria::all()]);
+
+        return new CategoriaCollection(Categoria::all());
     }
 }
