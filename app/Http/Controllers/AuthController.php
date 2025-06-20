bb4fe7ac442fdf59c58bf10be9896e539aabdcce
albertosanchezc <albertosanchezc98@gmail.com>
2025-06-19 22:12:46 -0600

    Añadiendo cors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -11,6 +12,17 @@ class AuthController extends Controller
     public function register(RegistroRequest $request){
         // Validar el registro
         $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        return [
+            'token' => $user->creteToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
     
     public function login(Request $request){
