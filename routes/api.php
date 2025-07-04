2441e7e77163d67836453c955a7a3637b490d4b1
albertosanchezc <albertosanchezc98@gmail.com>
2025-07-03 22:58:22 -0600

    Marcar producto como agotado y actualizando el DOM

--- routes/api.php
+++ routes/api.php
@@ -8,20 +8,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->group(function() {
-    Route::get('/user', function (Request $request){
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
     });
-    Route::post('/logout',[AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
     // Almacenar ordenes
-    Route::apiResource('/pedidos',PedidoController::class);
-
+    Route::apiResource('/pedidos', PedidoController::class);
+    Route::apiResource('/categorias', CategoriaController::class);
+    Route::apiResource('/productos', ProductoController::class);
 });
 
-Route::apiResource('/categorias',CategoriaController::class);
-Route::apiResource('/productos',ProductoController::class);
+
 
 // Autenticación
-Route::post('/registro',[AuthController::class, 'register']);
-Route::post('/login',[AuthController::class, 'login']);
+Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
